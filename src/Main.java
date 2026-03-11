ed53a2116e0e07d18a41bc6a88ddb3eb154b414b
Riccardo Gulin <riccardo.gulin@gmail.com>
2026-03-11 11:05:56 +0100

    Adds constructors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import entities.Circle;
+
 public class Main {
 	public static void main(String[] args) {
-		
+
+		Circle circle1 = new Circle(); // III COSTRUTTORE
+		Circle circle2 = new Circle(5.3); // II COSTRUTTORE
+		Circle circle3 = new Circle(0.2, 2.3, 10); // I COSTRUTTORE
+		Circle circle4 = new Circle(20); // II COSTRUTTORE
+
+//		System.out.println("Cerchio 1");
+//		circle1.printInfo();
+//		System.out.println("Cerchio 2");
+//		circle2.printInfo();
+//		System.out.println("Cerchio 3");
+//		circle3.printInfo();
+//		System.out.println("Cerchio 4");
+//		circle4.printInfo();
+
+		System.out.println("L'area del cerchio 1 é:" + circle1.getArea());
+		System.out.println("La coordinata X di cerchio 1 è: " + circle1.x);
+		circle1.x = 1000;
+		System.out.println("La coordinata X di cerchio 1 è: " + circle1.x);
 	}
 }
--- src/entities/Circle.java
+++ src/entities/Circle.java
@@ -6,6 +6,25 @@ public class Circle {
 	public double x;
 	public double y;
 
+	// LISTA COSTRUTTORI
+	public Circle(double coordX, double coordY, double raggio) { // I COSTRUTTORE <-- Circle c1 = new Circle(1.0, 0.5, 10.3);
+		x = coordX;
+		y = coordY;
+		radius = raggio;
+	}
+
+	public Circle(double raggio) { // II COSTRUTTORE <-- Circle c2 = new Circle(10.1);
+		radius = raggio;
+		x = 0.0;
+		y = 0.0;
+	}
+
+	public Circle() { // III COSTRUTTORE <-- Circle c3 = new Circle();
+		radius = 1.0;
+		x = 0.0;
+		y = 0.0;
+	}
+
 	// LISTA METODI
 	public double getDiameter() {
 		return radius * 2;
